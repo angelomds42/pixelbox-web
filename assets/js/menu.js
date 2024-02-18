436e4b7859d6a88b5875d1811b03af50c23518db
unknown <angelomds42@gmail.com>
2024-02-18 20:25:09 -0300

    Enable tab indicator

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,3 +6,19 @@ menuicon.addEventListener("click", () => {
     menuicon.classList.toggle("active");
     menumobile.classList.toggle("active");
 });
+
+var tabs_indicator = document.querySelector('.teste');
+
+var item = document.querySelectorAll('.navbar_menu_text');
+
+function indicator(e){
+    tabs_indicator.style.left = e.offsetLeft+"px";
+    tabs_indicator.style.width = e.offsetWidth+"px";
+}
+
+item.forEach(link => {
+    link.addEventListener("click", (e)=>{
+        indicator(e.target);
+    })
+})
+
