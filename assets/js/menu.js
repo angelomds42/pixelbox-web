abfb42568ed55bca34acb5fd145418ab58ce1d33
angelomds42 <angelomds42@gmail.com>
2024-02-20 13:54:19 -0300

    Fix mobile menu bug

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,65 +1,46 @@
-const menuicon = document.querySelector(".navbar_button");
+const menuIcon = document.querySelector(".navbar_button");
+const menuMobile = document.querySelector(".navbar_menu");
+const menuItems = document.querySelectorAll(".navbar_menu_text");
 
-const menumobile = document.querySelector(".navbar_menu");
-
-menuicon.addEventListener("click", () => {
-  menuicon.classList.toggle("active");
-  menumobile.classList.toggle("active");
-});
-
-// Função para selecionar a aba e mostrar o conteúdo associado
-function selectAndShow(element, tabClass) {
-  // Chama a função para selecionar a opção
-  selectOption(element);
-
-  // Chama a função para mostrar o conteúdo associado à tab
-  showContent(tabClass);
+// Função para fechar o menu
+function fecharMenu() {
+  menuIcon.classList.remove("active");
+  menuMobile.classList.remove("active");
 }
 
-// Função para selecionar a opção
-function selectOption(option) {
-  var options = document.querySelectorAll(".navbar_menu_text");
-  options.forEach(function (item) {
+// Adiciona um event listener para o ícone do menu
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  menuMobile.classList.toggle("active");
+});
+
+// Adiciona um event listener para cada item do menu
+menuItems.forEach(item => {
+  item.addEventListener("click", fecharMenu);
+});
+
+// Função para selecionar a opção e mostrar o conteúdo associado
+function selectAndShow(option, tabClass) {
+  // Remove a classe "selected" de todas as opções
+  menuItems.forEach(item => {
     item.classList.remove("selected");
   });
+  // Adiciona a classe "selected" à opção selecionada
   option.classList.add("selected");
-}
-
-// Função para mostrar o conteúdo associado à tab
-function showContent(tabClass) {
+  
   // Oculta todos os conteúdos das tabs
   var tabContents = document.querySelectorAll(".conteudo");
-  tabContents.forEach(function (content) {
+  tabContents.forEach(content => {
     content.style.display = "none";
   });
-
-  // Mostra o conteúdo da tab selecionada
+  
+  // Mostra o conteúdo associado à tab selecionada
   var selectedTabContent = document.querySelector("." + tabClass);
-  if (selectedTabContent) {
-    selectedTabContent.style.display = "block";
-  }
+  selectedTabContent.style.display = "flex"; // ou "block", dependendo do estilo que você deseja aplicar
 }
 
-// Chama a função selectAndShow para selecionar e mostrar a Tab 1 ao carregar o site
-window.onload = function () {
-  var tab1 = document.querySelector(".navbar_menu_text:nth-child(1)");
-  selectAndShow(tab1, "section-01_container");
+// Seleciona e mostra a seção section-01 ao carregar o site
+window.onload = function() {
+  var firstMenuItem = document.querySelector(".navbar_menu_text:nth-child(1)");
+  selectAndShow(firstMenuItem, "section-01_container");
 };
-
-// Atualiza o conteúdo da aba ao clicar em outra aba
-document.querySelectorAll(".navbar_menu_text").forEach(function (item) {
-  item.addEventListener("click", function () {
-    var tabClass = this.getAttribute("data-tab");
-    showContent(tabClass);
-
-    // Mostra todas as outras opções de conteúdo
-    var tabContents = document.querySelectorAll(".conteudo");
-    tabContents.forEach(function (content) {
-      if (content.classList.contains(tabClass)) {
-        content.style.display = "flex";
-      } else {
-        content.style.display = "none";
-      }
-    });
-  });
-});
