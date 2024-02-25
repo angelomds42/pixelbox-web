df9d377b2762b165c28664507b6332b252b51e8e
angeloxzs <angelomds42@gmail.com>
2024-02-25 14:20:00 -0300

    Cleanup and organization

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -15,32 +15,32 @@ menuIcon.addEventListener("click", () => {
 });
 
 // Adiciona um event listener para cada item do menu
-menuItems.forEach(item => {
+menuItems.forEach((item) => {
   item.addEventListener("click", fecharMenu);
 });
 
 // Função para selecionar a opção e mostrar o conteúdo associado
 function selectAndShow(option, tabClass) {
   // Remove a classe "selected" de todas as opções
-  menuItems.forEach(item => {
+  menuItems.forEach((item) => {
     item.classList.remove("selected");
   });
   // Adiciona a classe "selected" à opção selecionada
   option.classList.add("selected");
-  
+
   // Oculta todos os conteúdos das tabs
   var tabContents = document.querySelectorAll(".conteudo");
-  tabContents.forEach(content => {
+  tabContents.forEach((content) => {
     content.style.display = "none";
   });
-  
+
   // Mostra o conteúdo associado à tab selecionada
   var selectedTabContent = document.querySelector("." + tabClass);
   selectedTabContent.style.display = "flex"; // ou "block", dependendo do estilo que você deseja aplicar
 }
 
 // Seleciona e mostra a seção section-01 ao carregar o site
-window.onload = function() {
+window.onload = function () {
   var firstMenuItem = document.querySelector(".navbar_menu_text:nth-child(1)");
-  selectAndShow(firstMenuItem, "section-01_container");
+  selectAndShow(firstMenuItem, "section-01");
 };
